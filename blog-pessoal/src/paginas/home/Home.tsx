acd51e9563ecdf2ebbe5245acf76a6461a3e3acf
henrimfrio674 <henri.felix.araujo@gmail.com>
2024-08-16 14:22:24 -0300

    aa

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import './Home.css';
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
 
-interface minhaProps {
-    title: string;
-    description: string;
-  }
-  
-function Home(props:minhaProps){
+
+
+const Home = () => {
+    let navigate = useNavigate()
     return (
-        <>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        </>
-    );
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
 }
 
-export default Home;
+export default Home
