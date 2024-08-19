bb811c3f15353f9af3bf365819bf297233eeded3
henrimfrio674 <henri.felix.araujo@gmail.com>
2024-08-19 15:38:55 -0300

    af

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -9,7 +9,7 @@ import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostage
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-indigo-500 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
